38eee4ca2e935edd96123903b8766125657b9c3a
Gurpreet Singh <PreetEnzo1@gmail.com>
2019-04-13 13:10:26 -0700

    removed email settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -143,13 +143,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-#Email Config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'preetenzo1@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+
 
 
 try:
